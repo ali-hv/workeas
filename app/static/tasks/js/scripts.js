e14b58540f17b48d6f1ed210228292bda332ff48
alihv <alihv5000@gmail.com>
2024-03-19 03:25:51 +0330

    add search functionality to search box

--- app/static/tasks/js/scripts.js
+++ app/static/tasks/js/scripts.js
@@ -1,16 +1,37 @@
-document.querySelectorAll('.box').forEach(box => {
-    box.addEventListener('click', () => {
-        if (box.classList.contains('open')) {
-           box.classList.remove('open');
+document.querySelectorAll('.task-box').forEach(task => {
+    task.addEventListener('click', () => {
+        if (task.classList.contains('open')) {
+           task.classList.remove('open');
         }
         else {
             // Close all dropdowns
-            document.querySelectorAll('.box').forEach(box => {
-                box.classList.remove('open');
+            document.querySelectorAll('.task-box').forEach(task => {
+                task.classList.remove('open');
             });
 
-            // Open dropdown of clicked box
-            box.classList.toggle('open');
+            // Open dropdown of clicked task-box
+            task.classList.toggle('open');
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('searchInput');
+    const tasks = document.querySelectorAll('.task-box');
+
+    searchInput.addEventListener('input', function() {
+        const searchTerm = this.value.toLowerCase().trim();
+
+        tasks.forEach(task => {
+            const taskTitle = task.querySelector('h2').innerText.toLowerCase();
+            const taskDescription = task.querySelector('p').innerText.toLowerCase();
+
+            if (taskTitle.includes(searchTerm) || taskDescription.includes(searchTerm)) {
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+        });
+    });
+});
+
