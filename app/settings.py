66a39a85cc9d39093e9bad3bb516e0b8f9aa811b
alihv <alihv5000@gmail.com>
2024-03-31 05:04:13 +0330

    create auth app

--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,18 @@
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 router = APIRouter()
 
 timezone = 'UTC'
 
 templates = Jinja2Templates(directory="app/templates")
+
+# JWT authentication
+SECRET_KEY = os.environ.get('SECRET_KEY', 'secret-key')
+ALGORITHM = os.environ.get('ALGORITHM', 'HS256')
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
